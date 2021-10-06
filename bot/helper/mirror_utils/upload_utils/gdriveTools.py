a7798ae6c2e86c1e672da5a23979347a9f2e882c
burleylp <91642589+burleylp@users.noreply.github.com>
2021-10-06 18:55:56 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -382,8 +382,6 @@ class GoogleDriveHelper:
 
 
     def clone(self, link):
-        if ',' in link:
-            self.cloneFiles(link)
         global SERVICE_ACCOUNT_INDEX
         self.is_cloning = True
         self.start_time = time.time()
@@ -482,12 +480,6 @@ class GoogleDriveHelper:
             return msg, ""
         return msg, InlineKeyboardMarkup(buttons.build_menu(2))
     
-    def cloneFiles(self, link):
-        if ',' in link:
-            links = link.split(',')
-            for x in links:
-                x = x.replace(" ", "")
-                self.clone(x)
  
     def cloneFolder(self, name, local_path, folder_id, parent_id):
         LOGGER.info(f"Syncing: {local_path}")
